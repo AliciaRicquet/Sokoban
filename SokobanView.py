061ddd802ef3a3b554d5773f9b2baf8913a5f265
Fardoux Benoit <benoit_fardoux@ens.univ-artois.fr>
2021-05-30 11:55:11 +0200

    ajout vue

--- SokobanView.py
+++ SokobanView.py
@@ -1,17 +1,27 @@
 from PyQt5.QtWidgets import QMainWindow
 
 
-class SokobanView(QMainWindow):
+class SokobanView1(QMainWindow):
     def __init__(self):
         super().__init__()
         self.controller = None
         self.model = None
 
-    def setController(self,controller):
+    def setController(self, controller):
         self.controller = controller
 
-    def setModel(self,model):
+    def setModel(self, model):
         self.model = model
 
 
+class SokobanView2(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.controller = None
+        self.model = None
 
+    def setController(self, controller):
+        self.controller = controller
+
+    def setModel(self, model):
+        self.model = model
