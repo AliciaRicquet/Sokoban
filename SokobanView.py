a717a5adde9c5c94913cea61399a1018055ee4d4
Baptiste <baptiste_lemattre@ens.univ-artois.fr>
2021-05-30 12:26:33 +0200

    debut_view_1

--- SokobanView.py
+++ SokobanView.py
@@ -1,3 +1,4 @@
+from PyQt5.QtGui import QPainter
 from PyQt5.QtWidgets import QMainWindow
 
 
@@ -12,6 +13,10 @@ class SokobanView1(QMainWindow):
 
     def setModel(self, model):
         self.model = model
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.drawRect(self.model.getCoordonneePerso()[0],self.model.getCoordonneePerso()[1],30,30)
 
 class SokobanView2(QMainWindow):
     def __init__(self):
