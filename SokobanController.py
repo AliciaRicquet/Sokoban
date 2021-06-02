d821c96364462240071fe60691578477b930c1ac
Fardoux Benoit <benoit_fardoux@ens.univ-artois.fr>
2021-06-02 11:29:31 +0200

    reparation mvc

--- SokobanController.py
+++ SokobanController.py
@@ -3,9 +3,24 @@ class SokobanController:
         self.model = None
         self.views = []
 
-    def addView(self,views):
+    def addView(self, views):
         self.views.append(views)
 
     def setModel(self, model):
         self.model = model
 
+    def upMovement(self):
+        self.model.setCoordoneePerso(self.model.getCoordoneePerso[0], self.model.getCoordoneePerso[1]
+                                     - self.model.getVitesseDeplacement)
+
+    def downMovement(self):
+        self.model.setCoordoneePerso(self.model.getCoordoneePerso[0], self.model.getCoordoneePerso[1] +
+                                     self.model.getVitesseDeplacement)
+
+    def rightMovement(self):
+        self.model.setCoordoneePerso(self.model.getCoordoneePerso[0] + self.model.getVitesseDeplacement,
+                                     self.model.getCoordoneePerso[1])
+
+    def leftMovement(self):
+        self.model.setCoordoneePerso(self.model.getCoordoneePerso[0] - self.model.getVitesseDeplacement,
+                                     self.model.getCoordoneePerso[1])
--- SokobanModel.py
+++ SokobanModel.py
@@ -22,21 +22,11 @@ class SokobanModel:
     def timeEvent(self, numberOfView):
         self.views[numberOfView].update()
 
-    def upMovement(self):
-        self.setCoordoneePerso(self.coordoneePerso[0], self.coordoneePerso[1] - self.vitesseDeplacement)
-
-    def downMovement(self):
-        self.setCoordoneePerso(self.coordoneePerso[0], self.coordoneePerso[1] + self.vitesseDeplacement)
-
-    def rightMovement(self):
-        self.setCoordoneePerso(self.coordoneePerso[0] + self.vitesseDeplacement, self.coordoneePerso[1])
-
-    def leftMovement(self):
-        self.setCoordoneePerso(self.coordoneePerso[0] - self.vitesseDeplacement, self.coordoneePerso[1])
+    def getVitesseDeplacement(self):
+        return self.vitesseDeplacement
 
     def addCaisse(self, coordonee):
         self.caisses.append(coordonee)
-
     def moveUpCaisse(self, numCaisse):
         self.caisses[numCaisse] = (self.caisses[numCaisse][0], self.caisses[numCaisse][1] - self.vitesseDeplacement)
 
