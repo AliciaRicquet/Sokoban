d2b896f22561bcd9787dff7548fa5c4923424c41
Baptiste <baptiste_lemattre@ens.univ-artois.fr>
2021-06-04 08:39:58 +0200

    ajout_keyspressevent

--- SokobanView.py
+++ SokobanView.py
@@ -18,16 +18,17 @@ class SokobanView1(QMainWindow):
         painter = QPainter(self)
         painter.drawRect(self.model.getCoordonneePerso()[0],self.model.getCoordonneePerso()[1],30,30)
 
-class SokobanView2(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.controller = None
-        self.model = None
-
-    def setController(self, controller):
-        self.controller = controller
-
-    def setModel(self, model):
-        self.model = model
-
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Up:
+            self.SokobanController.changeDirection((0,-1))
+            print((0,-1))
+        elif e.key() == Qt.Key_Down:
+            self.controller.changeDirection((0,1))
+            print((0,1))
+        elif e.key() == Qt.Key_Right:
+            self.controller.changeDirection((-1,0))
+            print((-1,0))
+        elif e.key() == Qt.Key_Left:
+            self.controller.changeDirection((1,0))
+            print((1,0))
 
