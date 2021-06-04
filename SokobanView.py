371d14d2162f4c58cd6d581aa221529694151d91
Fardoux Benoit <benoit_fardoux@ens.univ-artois.fr>
2021-06-04 08:33:30 +0200

    suppression vue 2

--- SokobanView.py
+++ SokobanView.py
@@ -18,16 +18,3 @@ class SokobanView1(QMainWindow):
         painter = QPainter(self)
         painter.drawRect(self.model.getCoordonneePerso()[0],self.model.getCoordonneePerso()[1],30,30)
 
-class SokobanView2(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.controller = None
-        self.model = None
-
-    def setController(self, controller):
-        self.controller = controller
-
-    def setModel(self, model):
-        self.model = model
-
-
