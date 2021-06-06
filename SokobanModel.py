a4f931394809f8f495431530179367872fa31127
Fardoux Benoit <benoit_fardoux@ens.univ-artois.fr>
2021-06-06 10:22:34 +0200

    ajout level 1 sous forme de matrice

--- SokobanModel.py
+++ SokobanModel.py
@@ -1,8 +1,11 @@
 from PyQt5.QtCore import QTimer
 
 
+# 0 = case libre   1 = joueur,, 2 = mur, 3 = trou(objectif rien de personnel hein), 4 caisse
 class SokobanModel:
-    def __init__(self, h, w):
+    def __init__(self, level):
+        assert level == 1 or level == 2
+
         self.views = []
         self.timer = QTimer()
         self.timer.timeout.connect(self.timeEvent)
@@ -10,8 +13,18 @@ class SokobanModel:
         self.vitesseDeplacement = 10
         self.caisses = []
         self.direction = (0, 1)
-        self.matrix = [[[0] for i in range(h)] for j in range(w)]
-        self.level = 0
+        self.level = level
+        if self.level == 1:
+            self.matrix = [
+                [0, 0, 2, 2, 2, 0, 0, 0],
+                [0, 0, 2, 3, 2, 0, 0, 0],
+                [0, 0, 2, 4, 2, 2, 2, 2],
+                [2, 2, 2, 0, 1, 4, 3, 2],
+                [2, 3, 0, 4, 4, 2, 2, 2],
+                [2, 2, 2, 2, 0, 2, 0, 0],
+                [0, 0, 0, 2, 3, 2, 0, 0],
+                [0, 0, 0, 2, 2, 2, 0, 0]
+            ]
 
     def setCoordoneePerso(self, x, y):
         self.coordoneePerso = (x, y)
@@ -43,12 +56,12 @@ class SokobanModel:
     def moveLeftCaisse(self, numCaisse):
         self.caisses[numCaisse] = (self.caisses[numCaisse][0] - self.vitesseDeplacement, self.caisses[numCaisse][1])
     '''
+
     def getLevel(self):
         return self.level
 
-    def setLevel(self,number):
+    def setLevel(self, number):
         self.level = number
 
     def getMatrix(self):
         return self.matrix
-
