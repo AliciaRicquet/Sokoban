5d1cb7b877058ff1f298143c18e34e5ea709dcf6
Fardoux Benoit <benoit_fardoux@ens.univ-artois.fr>
2021-06-07 09:33:37 +0200

    ajout level 1 sous forme de matrice

--- SokobanView.py
+++ SokobanView.py
@@ -22,31 +22,28 @@ class SokobanView1(QMainWindow):
         self.SokobanController = None
         self.model = None
 
-
     def setController(self, controller):
         self.SokobanController = controller
 
     def setModel(self, model):
         self.model = model
-        self.setFixedSize(len(self.model.getMatrix()[0]*30),len(self.model.getMatrix()*30))
-
+        self.setFixedSize(len(self.model.getMatrix()[0] * 30), len(self.model.getMatrix() * 30))
 
     def paintEvent(self, event):
         print(self.model.getMatrix())
         painter = QPainter(self)
-        painter.drawRect(self.model.getCoordonneePerso()[0],self.model.getCoordonneePerso()[1],30,30)
+        painter.drawRect(self.model.getCoordonneePerso()[0], self.model.getCoordonneePerso()[1], 30, 30)
 
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Up:
-            self.SokobanController.changeDirection((0,-1))
-            print((0,-1))
+            self.SokobanController.changeDirection((0, -1))
+            print((0, -1))
         elif e.key() == Qt.Key_Down:
             self.SokobanController.changeDirection((0, 1))
-            print((0,1))
+            print((0, 1))
         elif e.key() == Qt.Key_Right:
             self.SokobanController.changeDirection((-1, 0))
-            print((-1,0))
+            print((-1, 0))
         elif e.key() == Qt.Key_Left:
             self.SokobanController.changeDirection((1, 0))
-            print((1 ,0))
-
+            print((1, 0))
