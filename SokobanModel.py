faf242095caeca93be0b3820a02a0413fed3936d
Fardoux Benoit <benoit_fardoux@ens.univ-artois.fr>
2021-06-13 11:01:38 +0200

    reglage erreur

--- SokobanModel.py
+++ SokobanModel.py
@@ -1,21 +1,21 @@
 from PyQt5.QtCore import QTimer
 
 
-# 0 = case libre   1 = joueur,, 2 = mur, 3 = trou(objectif rien de personnel hein), 4 caisse
+# 0 = case libre   1 = joueur, 2 = mur, 3 = trou(objectif rien de personnel hein), 4 caisse
 class SokobanModel:
     def __init__(self, level):
         assert level == 1 or level == 2
 
-        self.views = []
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.timeEvent)
-
-        self.vitesseDeplacement = 10
-        self.caisses = []
-        self.direction = (0, 1)
-        self.level = level
-        if self.level == 1:
-            self.matrix = [
+        self.__views = []
+        self.__timer = QTimer()
+        self.__timer.timeout.connect(self.timeEvent)
+        self.__vitesseDeplacement = 10
+        self.__caisses = []
+        self.__direction = (0, 1)
+        self.__level = level
+        self.__tabGoal = []
+        if self.__level == 1:
+            self.__matrix = [
                 [0, 0, 2, 2, 2, 0, 0, 0],
                 [0, 0, 2, 3, 2, 0, 0, 0],
                 [0, 0, 2, 4, 2, 2, 2, 2],
@@ -25,41 +25,51 @@ class SokobanModel:
                 [0, 0, 0, 2, 3, 2, 0, 0],
                 [0, 0, 0, 2, 2, 2, 0, 0]
             ]
-            self.coordoneePerso = (3, 4)
+        elif self.__level == 2:
+            self.__matrix = [
+                [0, 0, 0, 2, 2, 2, 2, 2, 0],
+                [0, 2, 2, 2, 0, 0, 0, 2, 0],
+                [0, 2, 3, 1, 4, 0, 0, 2, 0],
+                [0, 2, 2, 2, 0, 4, 3, 2, 0],
+                [0, 2, 3, 2, 2, 4, 0, 2, 2],
+                [0, 2, 0, 2, 0, 3, 0, 0, 2],
+                [0, 2, 4, 0, 0, 4, 4, 3, 2],
+                [0, 2, 0, 0, 0, 3, 0, 0, 2],
+                [0, 2, 2, 2, 2, 2, 2, 2, 2]
+            ]
+        for i in range(len(self.__matrix)):
+            for j in range(len(self.__matrix[0])):
+                if self.__matrix[i][j] == 3:
+                    self.__tabGoal.append((i, j))
+                if self.__matrix[i][j] == 1:
+                    self.__coordonneePerso = (i, j)
+
+    def getTabGoal(self):
+        return self.__tabGoal
 
     def getCoordonneePerso(self):
-        return self.coordoneePerso
+        return self.__coordonneePerso
 
     def addView(self, views):
-        self.views.append(views)
+        self.__views.append(views)
 
     def timeEvent(self, numberOfView):
-        self.views[numberOfView].update()
+        self.__views[numberOfView].update()
 
     def getVitesseDeplacement(self):
-        return self.vitesseDeplacement
+        return self.__vitesseDeplacement
+
+    def setMatrix(self):
+        self.__views.update()
 
     def addCaisse(self, coordonee):
-        self.caisses.append(coordonee)
-
-    '''def moveUpCaisse(self, numCaisse):
-        self.caisses[numCaisse] = (self.caisses[numCaisse][0], self.caisses[numCaisse][1] - self.vitesseDeplacement)
-
-    def moveDownCaisse(self, numCaisse):
-        self.caisses[numCaisse] = (self.caisses[numCaisse][0], self.caisses[numCaisse][1] + self.vitesseDeplacement)
-
-    def moveRightCaisse(self, numCaisse):
-        self.caisses[numCaisse] = (self.caisses[numCaisse][0] + self.vitesseDeplacement, self.caisses[numCaisse][1])
-
-    def moveLeftCaisse(self, numCaisse):
-        self.caisses[numCaisse] = (self.caisses[numCaisse][0] - self.vitesseDeplacement, self.caisses[numCaisse][1])
-    '''
+        self.__caisses.append(coordonee)
 
     def getLevel(self):
-        return self.level
+        return self.__level
 
     def setLevel(self, number):
-        self.level = number
+        self.__level = number
 
     def getMatrix(self):
-        return self.matrix
+        return self.__matrix
