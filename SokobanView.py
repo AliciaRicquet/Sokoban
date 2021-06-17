e81505b47eb0b63c081f88ed78ef3963b60bb63a
Fardoux Benoit <benoit_fardoux@ens.univ-artois.fr>
2021-06-17 07:48:39 +0200

    debug perso

--- SokobanView.py
+++ SokobanView.py
@@ -32,8 +32,7 @@ class SokobanView(QMainWindow):
         self.setCentralWidget(self.__window)
         self.setWindowIcon(QIcon("sprites/mur3.png"))
         size_ecran = QDesktopWidget().screenGeometry()
-        self.move((size_ecran.width() - self.geometry().width()) / 3,
-                  (size_ecran.height() - self.geometry().height()) / 10)
+        self.move((size_ecran.width() - self.geometry().width()) / 3, (size_ecran.height() - self.geometry().height())/10)
         # Declaration du grid Layout
         self.__grid = QGridLayout()
         self.__grid.setSpacing(0)
@@ -47,15 +46,8 @@ class SokobanView(QMainWindow):
         self.playlist.setPlaybackMode(QMediaPlaylist.Loop)
         self.musiqueSound = QMediaPlayer()
         self.musiqueSound.setPlaylist(self.playlist)
-        self.musiqueSound.play()
         # choix texture
         self.selectionTexture = randint(1, 5)
-        self.__player = None
-        self.__wall = None
-        self.__caisse = None
-        self.__hole = None
-        self.__ground = None
-        self.__grass = None
 
     def setController(self, controller):
         self.__controller = controller
@@ -63,11 +55,15 @@ class SokobanView(QMainWindow):
     def setModel(self, model):
         # Attribution du model
         self.__model = model
+        # creation d' un label
+
         # recupération de la matrice du model
         matrix = self.__model.getMatrix()
         # configuration de la taille de la fenêtre
         self.setFixedSize(len(matrix[0] * 100), len(matrix * 100))
-        self.creationLayout()
+        self.update()
+
+        self.musiqueSound.play()
 
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Up:
@@ -85,17 +81,14 @@ class SokobanView(QMainWindow):
         else:
             return
 
-    # fonction pour attribuer les textures
-    def attribuerTexture(self):
+    def update(self):
         matrix = self.__model.getMatrix()
-
         # configuration et choix des textures
-        # images statiques
+
         imageJoueur = QImage("./sprites/perso_bas.png", 'png')
         imageTrou = QImage("./sprites/hole.png")
         imageSol = QImage("sprites/ground_interior.png")
         imageGrass = QImage("sprites/grass.png")
-        # image aléatoire
         if self.selectionTexture == 1:
             imageCaisse = QImage("./sprites/caisse_valide1.png", 'png')
             imageMur = QImage("./sprites/mur1.png", 'png')
@@ -115,59 +108,39 @@ class SokobanView(QMainWindow):
             imageCaisse = QImage("./sprites/caisse_valide5.png", 'png')
             imageMur = QImage("./sprites/mur5.png", 'png')
 
+
         # attribution de la texture au joueur au mur et a la caisse
-        # taille des textures
         w = self.width() / len(matrix)
         h = self.height() / len(matrix[0])
-        # attribution des pixmap
-        self.__player = QPixmap(imageJoueur.scaled(w, h))
-        self.__wall = QPixmap(imageMur.scaled(w, h))
-        self.__caisse = QPixmap(imageCaisse.scaled(w, h))
-        self.__hole = QPixmap(imageTrou.scaled(w, h))
-        self.__ground = QPixmap(imageSol.scaled(w, h))
-        self.__grass = QPixmap(imageGrass.scaled(w, h))
 
-    def creationLayout(self):
-        matrix = self.__model.getMatrix()
-        self.attribuerTexture()
-        # ajout des textures au jeu
+        joueur = QPixmap(imageJoueur.scaled(w, h))
+        wall = QPixmap(imageMur.scaled(w, h))
+        caisse = QPixmap(imageCaisse.scaled(w, h))
+        trou = QPixmap(imageTrou.scaled(w, h))
+        sol = QPixmap(imageSol.scaled(w,h))
+        grass = QPixmap(imageGrass.scaled(w,h))
         for i in range(len(matrix)):
-            tmp = []
             for j in range(len(matrix[i])):
                 label = QLabel()
-                if matrix[i][j] == 0:
-                    label.setPixmap(self.__ground)
+                if matrix[i][j] == 0 :
+                    label.setPixmap(sol)
                     self.__grid.addWidget(label, i, j)
-                    tmp.append(label)
-
-                elif matrix[i][j] == 1:
-                    print(matrix[i][j])
-                    label.setPixmap(self.__player)
-                    self.__grid.addWidget(label, i, j)
-                    tmp.append(label)
-
                 elif matrix[i][j] == 2:
-                    label.setPixmap(self.__wall)
+                    label.setPixmap(wall)
                     self.__grid.addWidget(label, i, j)
-                    tmp.append(label)
-
                 elif matrix[i][j] == 3:
-                    label.setPixmap(self.__hole)
+                    label.setPixmap(trou)
                     self.__grid.addWidget(label, i, j)
-                    tmp.append(label)
 
                 elif matrix[i][j] == 4:
-                    label.setPixmap(self.__caisse)
+                    label.setPixmap(caisse)
                     self.__grid.addWidget(label, i, j)
-                    tmp.append(label)
-
                 elif matrix[i][j] == 5:
-                    label.setPixmap(self.__grass)
+                    label.setPixmap(grass)
                     self.__grid.addWidget(label, i, j)
-                    tmp.append(label)
-            self.__labelGrid.append(tmp)
-        self.afficherJoueur(self.__labelGrid)
 
-    def afficherJoueur(self,matrix):
-        posJoueur = self.__model.getCoordonneePerso()
-        matrix[posJoueur[0]][posJoueur[1]].setPixmap(self.__player)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if i == self.__model.getCoordonneePerso()[0] and j == self.__model.getCoordonneePerso()[1]:
+                    label.setPixmap(joueur)
+                    self.__grid.addWidget(label, i, j)
