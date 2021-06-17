394c9f3467c81c692d0aae98dd42d2317ebf58fd
Baptiste <baptiste_lemattre@ens.univ-artois.fr>
2021-06-17 20:04:10 +0200

    objectif

--- SokobanView.py
+++ SokobanView.py
@@ -56,6 +56,7 @@ class SokobanView(QMainWindow):
         self.__grass = None
         self.caisse = QSound("son/bruitCaisse.wav")
         self.victoire = QSound("son/victoire.wav")
+        self.bonEndroit=QSound("son/objectif.wav")
 
     def confImage(self):
         matrix = self.__model.getMatrix()
@@ -184,3 +185,6 @@ class SokobanView(QMainWindow):
 
     def victoireSon(self):
         self.victoire.play()
+
+    def objectif(self):
+        self.objectif.play()
