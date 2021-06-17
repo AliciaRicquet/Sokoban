52ee20e5a85418f03573ea23163cbaf04f1fce6e
Fardoux Benoit <benoit_fardoux@ens.univ-artois.fr>
2021-06-17 17:14:59 +0200

    JEUX FONCTIONNEL MANQUE PLUS QUE FAIRE UN ECRAN DE VICTOIRE

--- SokobanController.py
+++ SokobanController.py
@@ -9,14 +9,12 @@ class SokobanController:
     def __init__(self):
         self.model = None
         self.view = None
-        self.__matrix = None
 
     def setView(self, view):
         self.view = view
 
     def setModel(self, model):
         self.model = model
-        self.__matrix = self.model.getMatrix()
 
     def movement(self, dir):
         perso = [[self.model.getCoordonneePerso()[0] + dir[0]], [self.model.getCoordonneePerso()[1] + dir[1]]]
@@ -27,7 +25,7 @@ class SokobanController:
                         [self.model.getCoordonneePerso()[0] + dir[0], self.model.getCoordonneePerso()[1] + dir[1]]
                     )
                     self.model.addPas()
-                    self.__matrix = perso
+
                     self.model.update(self.model.getMatrix())
                 elif not self.verifCaisses(dir):
                     if self.verifMurCaisse(dir):
@@ -35,26 +33,25 @@ class SokobanController:
                             print("caisse avant ", self.model.getCaisse())
                             indice = self.obtenirLaCaisseABouger(dir)
                             self.model.modifierCaisse(
-                                indice,[
-                                    self.model.getCaisse()[indice][0] + dir[0], self.model.getCaisse()[indice][1] + dir[1]
+                                indice, [
+                                    self.model.getCaisse()[indice][0] + dir[0],
+                                    self.model.getCaisse()[indice][1] + dir[1]
                                 ])
                             self.model.setCoordoneePerso(
-                                [self.model.getCoordonneePerso()[0] + dir[0], self.model.getCoordonneePerso()[1] + dir[1]]
+                                [self.model.getCoordonneePerso()[0] + dir[0],
+                                 self.model.getCoordonneePerso()[1] + dir[1]]
                             )
                             print("caisse après", self.model.getCaisse())
                             self.model.addPas()
-                            self.__matrix = perso
                             self.model.update(self.model.getMatrix())
                     print(self.model.getPas())
 
-
-    def obtenirLaCaisseABouger(self,coo):
+    def obtenirLaCaisseABouger(self, coo):
         caisse = self.model.getCaisse()
         perso = self.model.getCoordonneePerso()
         for i in range(len(caisse)):
-            if perso[0] + coo[0] == caisse[i][0] and  perso[1] + coo[1] == caisse[i][1]:
+            if perso[0] + coo[0] == caisse[i][0] and perso[1] + coo[1] == caisse[i][1]:
                 return i
-
 
     def verifMurPerso(self, dir):
         matrice = self.model.getMatrix()
@@ -63,14 +60,14 @@ class SokobanController:
             return True
         return False
 
-    def verifMurCaisse(self,dir):
+    def verifMurCaisse(self, dir):
         matrice = self.model.getMatrix()
         cooPerso = self.model.getCoordonneePerso()
-        if matrice[cooPerso[0] + (dir[0])*2][cooPerso[1] + (dir[1])*2] != 2:
+        if matrice[cooPerso[0] + (dir[0]) * 2][cooPerso[1] + (dir[1]) * 2] != 2:
             return True
         return False
 
-    def verifCaisseCaisse(self,dir):
+    def verifCaisseCaisse(self, dir):
         cooPerso = self.model.getCoordonneePerso()
         for element in self.model.getCaisse():
             if element[0] == cooPerso[0] + (dir[0]) * 2 and element[1] == cooPerso[1] + (dir[1]) * 2:
@@ -81,7 +78,7 @@ class SokobanController:
         cooPerso = self.model.getCoordonneePerso()
         cooCaisse = self.model.getCaisse()
         for elements in cooCaisse:
-            if  elements[0] == cooPerso[0]+dir[0] and elements[1] == cooPerso[1]+dir[1]:
+            if elements[0] == cooPerso[0] + dir[0] and elements[1] == cooPerso[1] + dir[1]:
                 return False
         return True
 
